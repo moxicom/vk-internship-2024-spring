bf56bd2a2bc956077e15d10a97a09b1121efbd07
Alexander Markov <45567992+moxicom@users.noreply.github.com>
2024-03-18 19:06:36 +0300

    fixed auth issue

--- internal/handlers/middleware.go
+++ internal/handlers/middleware.go
@@ -28,15 +28,14 @@ func (h *Handler) IsAdminAuth(w http.ResponseWriter, r *http.Request, next http.
 func (h *Handler) IsBasicUserAuth(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	username, password, ok := r.BasicAuth()
 	if ok {
-
-		// TODO: Get from database here
-		expectedUsername := "user"
-		expectedPassword := "password"
-
-		usernameMatch := username == expectedUsername
-		passwordMatch := password == expectedPassword
-
-		if usernameMatch && passwordMatch {
+		// You can insert something more here
+		match, err := h.service.Users.CheckUser(username, password, false)
+		if err != nil {
+			h.log.Error(err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if match {
 			next.ServeHTTP(w, r)
 			return
 		}
--- internal/storage/postgres/users_storage/users_storage.go
+++ internal/storage/postgres/users_storage/users_storage.go
@@ -12,13 +12,21 @@ func New(db *sql.DB) *UsersStorage {
 
 func (s *UsersStorage) CheckUser(username, password string, isAdmin bool) (bool, error) {
 	var count int
-	err := s.db.QueryRow(
-		"SELECT COUNT(*) FROM users WHERE username=$1"+
-			" AND password_hash=$2 AND isAdmin=$3",
-		username,
-		password,
-		isAdmin).Scan(&count)
-
+	var err error
+	if isAdmin {
+		err = s.db.QueryRow(
+			"SELECT COUNT(*) FROM users WHERE username=$1"+
+				" AND password_hash=$2 AND isAdmin=$3",
+			username,
+			password,
+			isAdmin).Scan(&count)
+	} else {
+		err = s.db.QueryRow(
+			"SELECT COUNT(*) FROM users WHERE username=$1"+
+				" AND password_hash=$2",
+			username,
+			password).Scan(&count)
+	}
 	if err != nil {
 		return false, err
 	}
